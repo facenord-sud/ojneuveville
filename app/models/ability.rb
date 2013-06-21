425b981c8ce7b49436ea847912759612d28632a3
leo <facenord.sud@gmail.com>
2013-06-22 00:13:28 +0200

    correction heroku

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,32 +6,35 @@ class Ability
     if user.nil?
       can :create, User
       return
-    end
-    if user.roles.empty?
+    elsif user.roles.empty?
       can :update, User
       can :read, User
-      return
-    end
-    user.roles.each do |role|
-      role.permissions.each do |permission|
-        if permission.subject_class.nil?
-          can permission.action.to_sym
-          return
+    elsif !user.roles.empty?
+      user.roles.each do |role|
+        role.permissions.each do |permission|
+          if permission.subject_class.nil?
+            can permission.action.to_sym
+            return
+          end
+          if !permission.un_constantize
+            sec_param = permission.subject_class.constantize
+          else
+            sec_param = permission.subject_class.to_sym
+          end
+          if permission.subject_id.nil?
+            can permission.action.to_sym, sec_param
+          elsif permission.all
+            can permission.action.to_sym, :all
+          elsif !permission.more.nil?
+            can permission.action.to_sym, sec_param
+          else
+            can permission.action.to_sym, sec_param, :id => permission.subject_id
+          end
         end
-        if !permission.un_constantize
-          sec_param = permission.subject_class.constantize
-        else
-          sec_param = permission.subject_class.to_sym
-        end
-        if permission.subject_id.nil?
-          can permission.action.to_sym, sec_param
-        elsif permission.all
-          can permission.action.to_sym, :all
-        elsif !permission.more.nil?
-          can permission.action.to_sym, sec_param
-        else
-          can permission.action.to_sym, sec_param, :id => permission.subject_id
-        end
+      end
+    else
+      if User.count == 1
+        can :manage, :all
       end
     end
   end
