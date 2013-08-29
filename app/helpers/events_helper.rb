9b1eecde759eb6f696a428786c9c7333ba5fe370
leo <facenord.sud@gmail.com>
2013-08-29 13:43:55 +0200

    déploiment heroku

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -3,7 +3,13 @@ module EventsHelper
 		"#{l(event.starting_at, :format => format)} #{t(:au)}  #{l(event.ending_at, :format => format)}"
   end
 
-  def print_responsable event
-    event.responsable || link_to(event.user.name, event.user) || 'Aucun organisateur'
+  def print_responsable(event)
+    if event.responsable.blank?
+      link_to(event.user.name, event.user)
+    elsif ! event.responsable.blank?
+      event.responsable
+    else
+      t('events.helper.no_responsable')
+    end
   end
 end
