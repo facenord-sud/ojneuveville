6bddcc8acf57c9b1fb3d7ead842a2a85c6bf5ed9
leo <facenord.sud@gmail.com>
2013-11-05 19:06:37 +0100

    email

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,11 +1,11 @@
 ActionMailer::Base.smtp_settings = {  
   :address              => "smtp.gmail.com",  
   :port                 => 587,  
-  :domain               => "damp-lowlands-7685.herokuapp.com",  
+  :domain               => "oj-laneuveville.herokuapp.com",
   :user_name            => "facenord.sud@gmail.com",  
   :password             => "montagne.12",  
   :authentication       => "plain",  
   :enable_starttls_auto => true  
 }
 
-ActionMailer::Base.default_url_options[:host] = "0.0.0.0:3000"
+ActionMailer::Base.default_url_options[:host] = "oj-laneuveville.herokuapp.com"
